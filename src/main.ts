e5ee3a95590531863c4377a8b5451c9d10d764f3
Carl Vuorinen <carl.vuorinen@gmail.com>
2018-10-23 09:09:36 +0300

    Install fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBasketballBall } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(require('vue-shortkey'))
+
+library.add(faBasketballBall, faGithub)
+Vue.component('font-awesome-icon', require('@fortawesome/vue-fontawesome').FontAwesomeIcon)
 
 new Vue({
   store,
